242f8da7560721c3276a99fdc581589ea20adc73
Alexander Svensson <gurkslask@gmail.com>
2016-02-17 08:44:08 +0100

    Did a lot of coding and need to make a new branch

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 )
 
 func main() {
@@ -12,4 +13,23 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("%s", content)
+	var content_string string
+	content_string = string(content)
+	var string_list []string
+	var result_list []string
+	var result_map map[string]int
+	for _, i := range content_string {
+		string_list = strings.Split(string(i), "\n")
+	}
+	for i, _ := range string_list {
+		result_list = strings.Split(string(i), " ")
+		for j, _ := range result_list {
+			if j[0] != "sudo" {
+				result_map[j[0]] += 1
+			} else {
+				result_map[j[1]] += 1
+			}
+		}
+	}
+	fmt.Sprintf("%s", string_list)
 }
